2ff06a242cb8cd049099c38a96dcfd7f28646966
祖耀 <1796381757@qq.com>
2023-09-11 19:46:30 +0800

    fix

---
+++ src/com/zy/encap/Encapsulation01.java
@@ -0,0 +1,37 @@
+package com.zy.encap;
+
+public class Encapsulation01 {
+    public static void main(String[] args) {
+        person jack = new person();
+
+    }
+}
+class person{
+    public String name;
+    private int age;
+    private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
